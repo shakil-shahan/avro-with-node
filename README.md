db65fe6950c47bfb81c8a0a1c079fa6dbd279847
Md. Shakil Mahmud Shahan <shakil.shahan@bs-23.net>
2021-02-02 21:35:35 +0600

    Script done for decript .avro and write decripted file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # avro-with-node
+
+## Requirements
+
+To run the project, you have to be installed Node and npm installed in your environement.
+
+### Install dependencies
+
+Redirect to the file directory and run the command
+
+> `npm install`
+
+## Setup Project
+
+Go to the `index.js` file and update the `filePath` value with your expected file location.
+
+**Example**
+
+`const filePath = '/home/root/collection/file.avro';`
+
+## Run the project
+
+> `npm start`
---
+++ index.js
@@ -0,0 +1,74 @@
+const avro = require('avsc');
+var fs = require('fs');
+var path = require('path');
+
+// ##################################################################
+// =================== Input File Location/Path Here ================
+
+const filePath = '<Input your File Path>';
+
+// Example
+// const filePath = '/home/root/collection/file.avro';
+
+// =================== Input File Location/Path Here ================
+// ##################################################################
+
+// AVSC Schema
+const sampleSenscomAzureSchema = {
+  type: 'array',
+  items: {
+    type: 'record',
+    fields: [
+      { name: '_id', type: 'string' },
+      { name: 'type', type: 'string' },
+      { name: 'timestamp', type: 'double' },
+      { name: 'gateway_id', type: 'string' },
+      { name: 'sensor_id', type: 'string' },
+      {
+        name: 'jsondata',
+        type: {
+          type: 'record',
+          fields: [
+            { name: 'Humid1', type: 'double' },
+            { name: 'Humid2', type: 'double' },
+            { name: 'Humid3', type: 'double' },
+            { name: 'Humid4', type: 'double' },
+            { name: 'Temp1', type: 'double' },
+            { name: 'Temp2', type: 'double' },
+            { name: 'Temp3', type: 'double' },
+            { name: 'Temp4', type: 'double' },
+            { name: 'Status', type: 'double' },
+          ],
+        },
+      },
+      { name: 'createdAt', type: { type: 'string', logicalType: 'date' } },
+    ],
+  },
+};
+
+// function to decode .avro file
+const decodeAvro = () => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.warn('Error =>', err);
+    } else {
+      try {
+        const type = avro.Type.forSchema(sampleSenscomAzureSchema);
+        const val = type.fromBuffer(data);
+        const decodedFileName = 'decoded/' + path.basename(filePath).split('.').slice(0, -1).join('.') + '.json';
+        fs.writeFile(decodedFileName, val, (err) => {
+          if (err) {
+            console.error('Error =>', err);
+          }
+          const savedFileLocation = __dirname + '/' + decodedFileName;
+          console.log({ data: val, message: `Decoded file path: ${savedFileLocation}` });
+        });
+      } catch (err) {
+        console.error('Error =>', err);
+      }
+    }
+  });
+};
+
+// call the function
+decodeAvro();
